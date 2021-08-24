557a3f493cfdc49cb7978d19f6a21c3c89ca82b4
0xenAlpha <x0xenx@gmail.com>
2021-08-24 16:44:21 +0300

    TRADE DATE QUANTITY

---
+++ lib/translation_templates.py
@@ -0,0 +1,81 @@
+class GCUS:
+
+	translate_dict={		
+		"TRANSACTION CODE":{
+			"position":[0,2],
+			"type":"X",
+			"operation":"",
+		},
+		"RECORD INDICATOR VALUE":{
+			"position":[2,3],
+			"type":"X",
+			"operation":"",
+		},
+		"RECORD ID SEQUENCE NUMBER":{
+			"position":[],
+			"type":"",
+			"operation":"",
+		},
+		"ACCOUNT NUMBER":{
+			"position":[],
+			"type":"",
+			"operation":"",
+		},
+		"CUSIP NUMBER":{
+			"position":[],
+			"type":"",
+			"operation":"",
+		},
+		"PORTFOLIO CURRENCY":{
+			"position":[],
+			"type":"",
+			"operation":"",
+		},
+		"Not Used 1":{
+			"position":[],
+			"type":"",
+			"operation":"",
+		},
+		"INVESTMENT PROFESSIONAL (IP) OF RECORD NUMBER":{
+			"position":[],
+			"type":"",
+			"operation":"",
+		},
+		"INTRODUCING BROKER DEALER (IBD) NUMBER":{
+			"position":[],
+			"type":"",
+			"operation":"",
+		},
+		"CURRENCY/SECURITY INDICATOR":{
+			"position":[],
+			"type":"",
+			"operation":"",
+		},
+		"ISSUE CURRENCY":{
+			"position":[],
+			"type":"",
+			"operation":"",
+		},
+		"DATE STAMP/TRADE DATE":{
+			"position":[],
+			"type":"",
+			"operation":"",
+		},
+		"DATE STAMP/SETTLEMENT DATE":{
+			"position":[],
+			"type":"",
+			"operation":"",
+		},
+		"TRADE DATE QUANTITY":{
+			"position":[],
+			"type":"",
+			"operation":"",
+		},
+		"":{
+			"position":[],
+			"type":"",
+			"operation":"",
+		}
+	}
+
+
